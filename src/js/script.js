517111e9eb4d215562d83650a77958a6e585feea
Samuel Reutcky <sreutcky@gmail.com>
2024-11-15 08:57:27 -0600

    Created score class

--- src/js/script.js
+++ src/js/script.js
@@ -9,38 +9,207 @@ function listen(event, selector, callback) {
 }
 
 const wordBank = [
-  'dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building',
-  'population', 'weather', 'bottle', 'history', 'dream', 'character', 'money',
-  'absolute', 'discipline', 'machine', 'accurate', 'connection', 'rainbow',
-  'bicycle', 'eclipse', 'calculator', 'trouble', 'watermelon', 'developer',
-  'philosophy', 'database', 'periodic', 'capitalism', 'abominable', 'phone',
-  'component', 'future', 'pasta', 'microwave', 'jungle', 'wallet', 'canada',
-  'velvet', 'potion', 'treasure', 'beacon', 'labyrinth', 'whisper', 'breeze',
-  'coffee', 'beauty', 'agency', 'chocolate', 'eleven', 'technology',
-  'alphabet', 'knowledge', 'magician', 'professor', 'triangle', 'earthquake',
-  'baseball', 'beyond', 'evolution', 'banana', 'perfume', 'computer',
-  'butterfly', 'discovery', 'ambition', 'music', 'eagle', 'crown',
-  'chess', 'laptop', 'bedroom', 'delivery', 'enemy', 'button', 'door', 'bird',
-  'superman', 'library', 'unboxing', 'bookstore', 'language', 'homework',
-  'beach', 'economy', 'interview', 'awesome', 'challenge', 'science',
-  'mystery', 'famous', 'league', 'memory', 'leather', 'planet', 'software',
-  'update', 'yellow', 'keyboard', 'window', 'beans', 'truck', 'sheep',
-  'blossom', 'secret', 'wonder', 'enchantment', 'destiny', 'quest', 'sanctuary',
-  'download', 'blue', 'actor', 'desk', 'watch', 'giraffe', 'brazil',
-  'audio', 'school', 'detective', 'hero', 'progress', 'winter', 'passion',
-  'rebel', 'amber', 'jacket', 'article', 'paradox', 'social', 'resort',
-  'mask', 'escape', 'promise', 'band', 'level', 'hope', 'moonlight', 'media',
-  'orchestra', 'volcano', 'guitar', 'raindrop', 'inspiration', 'diamond',
-  'illusion', 'firefly', 'ocean', 'cascade', 'journey', 'laughter', 'horizon',
-  'exploration', 'serendipity', 'infinity', 'silhouette', 'wanderlust',
-  'marvel', 'nostalgia', 'serenity', 'reflection', 'twilight', 'harmony',
-  'symphony', 'solitude', 'essence', 'melancholy', 'melody', 'vision',
-  'silence', 'whimsical', 'eternity', 'cathedral', 'embrace', 'poet', 'ricochet',
-  'mountain', 'dance', 'sunrise', 'dragon', 'adventure', 'galaxy', 'echo',
-  'fantasy', 'radiant', 'serene', 'legend', 'starlight', 'light', 'pressure',
-  'bread', 'cake', 'caramel', 'juice', 'mouse', 'charger', 'pillow', 'candle',
-  'film', 'jupiter'
-  ];
+  'dinosaur',
+  'love',
+  'pineapple',
+  'calendar',
+  'robot',
+  'building',
+  'population',
+  'weather',
+  'bottle',
+  'history',
+  'dream',
+  'character',
+  'money',
+  'absolute',
+  'discipline',
+  'machine',
+  'accurate',
+  'connection',
+  'rainbow',
+  'bicycle',
+  'eclipse',
+  'calculator',
+  'trouble',
+  'watermelon',
+  'developer',
+  'philosophy',
+  'database',
+  'periodic',
+  'capitalism',
+  'abominable',
+  'phone',
+  'component',
+  'future',
+  'pasta',
+  'microwave',
+  'jungle',
+  'wallet',
+  'canada',
+  'velvet',
+  'potion',
+  'treasure',
+  'beacon',
+  'labyrinth',
+  'whisper',
+  'breeze',
+  'coffee',
+  'beauty',
+  'agency',
+  'chocolate',
+  'eleven',
+  'technology',
+  'alphabet',
+  'knowledge',
+  'magician',
+  'professor',
+  'triangle',
+  'earthquake',
+  'baseball',
+  'beyond',
+  'evolution',
+  'banana',
+  'perfume',
+  'computer',
+  'butterfly',
+  'discovery',
+  'ambition',
+  'music',
+  'eagle',
+  'crown',
+  'chess',
+  'laptop',
+  'bedroom',
+  'delivery',
+  'enemy',
+  'button',
+  'door',
+  'bird',
+  'superman',
+  'library',
+  'unboxing',
+  'bookstore',
+  'language',
+  'homework',
+  'beach',
+  'economy',
+  'interview',
+  'awesome',
+  'challenge',
+  'science',
+  'mystery',
+  'famous',
+  'league',
+  'memory',
+  'leather',
+  'planet',
+  'software',
+  'update',
+  'yellow',
+  'keyboard',
+  'window',
+  'beans',
+  'truck',
+  'sheep',
+  'blossom',
+  'secret',
+  'wonder',
+  'enchantment',
+  'destiny',
+  'quest',
+  'sanctuary',
+  'download',
+  'blue',
+  'actor',
+  'desk',
+  'watch',
+  'giraffe',
+  'brazil',
+  'audio',
+  'school',
+  'detective',
+  'hero',
+  'progress',
+  'winter',
+  'passion',
+  'rebel',
+  'amber',
+  'jacket',
+  'article',
+  'paradox',
+  'social',
+  'resort',
+  'mask',
+  'escape',
+  'promise',
+  'band',
+  'level',
+  'hope',
+  'moonlight',
+  'media',
+  'orchestra',
+  'volcano',
+  'guitar',
+  'raindrop',
+  'inspiration',
+  'diamond',
+  'illusion',
+  'firefly',
+  'ocean',
+  'cascade',
+  'journey',
+  'laughter',
+  'horizon',
+  'exploration',
+  'serendipity',
+  'infinity',
+  'silhouette',
+  'wanderlust',
+  'marvel',
+  'nostalgia',
+  'serenity',
+  'reflection',
+  'twilight',
+  'harmony',
+  'symphony',
+  'solitude',
+  'essence',
+  'melancholy',
+  'melody',
+  'vision',
+  'silence',
+  'whimsical',
+  'eternity',
+  'cathedral',
+  'embrace',
+  'poet',
+  'ricochet',
+  'mountain',
+  'dance',
+  'sunrise',
+  'dragon',
+  'adventure',
+  'galaxy',
+  'echo',
+  'fantasy',
+  'radiant',
+  'serene',
+  'legend',
+  'starlight',
+  'light',
+  'pressure',
+  'bread',
+  'cake',
+  'caramel',
+  'juice',
+  'mouse',
+  'charger',
+  'pillow',
+  'candle',
+  'film',
+  'jupiter',
+];
 
 const availableWords = [...wordBank];
 
@@ -60,6 +229,30 @@ let correctHits = 0;
 const correctSound = select('#correct-sound');
 
 const gameMusic = select('.gameMusic');
+
+class Score {
+  #date;
+  #hits;
+  #percentage;
+
+  constructor(date, hits, percentage) {
+    this.#date = date;
+    this.#hits = hits;
+    this.#percentage = ((percentage / 200) * 100).toFixed(2);
+  }
+
+  get date() {
+    return this.#date;
+  }
+
+  get hits() {
+    return this.#hits;
+  }
+
+  get percentage() {
+    return this.#percentage;
+  }
+}
 
 function playMusic() {
   gameMusic.play();
@@ -142,6 +335,8 @@ function startGame() {
 }
 
 function gameOver() {
+  const score = new Score(new Date(), correctHits, correctHits);
+  console.log(score);
   time.innerText = '---';
   input.disabled = true;
   input.value = '';
@@ -155,6 +350,8 @@ function gameOver() {
 }
 
 function restartGame() {
+  const score = new Score(new Date(), correctHits, correctHits);
+  console.log(score);
   input.placeholder = 'Restarting...';
   input.style.backgroundColor = '#ca6262';
   time.innerText = '---';
